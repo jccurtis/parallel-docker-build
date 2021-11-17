638a7e38b31e96ba374e3b04249e97d8c7aa7d28
Joey Curtis <joseph.c.curtis@gmail.com>
2021-11-17 13:07:55 -0800

    Basic package structure and setup

---
+++ parallel_docker_build/__metadata__.py
@@ -0,0 +1,40 @@
+"""parallel_docker_build package metadata."""
+
+__name__ = "parallel_docker_build"
+__author__ = "Joseph (Joey) Curtis"
+__email__ = "Contact author via github"
+__description__ = "parallel_docker_build"
+__url__ = "https://gitlab.com/jccurtis/parallel-docker-build"
+__version__ = "0.1.0"
+# classifiers from list at https://pypi.org/classifiers/
+__classifiers__ = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Science/Research",
+    "Operating System :: OS Independent",
+    "Natural Language :: English",
+    "License :: MIT",
+]
+__license__ = "MIT"
+__copyright__ = """\
+Copyright (c) 2021 Joseph (Joey) Curtis
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
