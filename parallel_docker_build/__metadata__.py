75cbdca3801bce4af6698772d929e87d0a1038eb
Joey Curtis <joseph.c.curtis@gmail.com>
2022-01-24 15:06:59 -0800

    Bump version: 0.1.4 → 0.2.0

--- parallel_docker_build/__metadata__.py
+++ parallel_docker_build/__metadata__.py
@@ -5,7 +5,7 @@ __author__ = "Joseph (Joey) Curtis"
 __email__ = "Contact author via github"
 __description__ = "parallel_docker_build"
 __url__ = "https://gitlab.com/jccurtis/parallel-docker-build"
-__version__ = "0.1.4"
+__version__ = "0.2.0"
 # classifiers from list at https://pypi.org/classifiers/
 __classifiers__ = [
     "Programming Language :: Python :: 3 :: Only",
